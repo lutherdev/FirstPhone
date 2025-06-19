2bfcf24835f6f02c50c0a62491f5db0670f4d0d6
lutherdev <devturd@gmail.com>
2025-06-19 08:52:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/firstphone/FirstScreen.kt
@@ -0,0 +1,29 @@
+package com.example.firstphone
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FirstScreen : AppCompatActivity() {
+    lateinit var btnProceed: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        btnProceed = findViewById<Button>(R.id.btnProceed)
+        btnProceed.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+    }
+}
