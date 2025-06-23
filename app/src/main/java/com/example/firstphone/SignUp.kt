ace22194effeda0a269e8ce31013d5bea61d61ba
lutherdev <devturd@gmail.com>
2025-06-23 08:20:53 +0800

    feat: App Update

--- app/src/main/java/com/example/firstphone/SignUp.kt
+++ app/src/main/java/com/example/firstphone/SignUp.kt
@@ -8,21 +8,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Login : AppCompatActivity() {
-    lateinit var btnSignup: Button
-
+class SignUp : AppCompatActivity() {
+    lateinit var btnLoginBack: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        btnSignup = findViewById<Button>(R.id.btnSignup)
-        btnSignup.setOnClickListener{
-            val intent = Intent(this, SignUp::class.java)
+        btnLoginBack = findViewById<Button>(R.id.btnLoginBack)
+        btnLoginBack.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
     }
