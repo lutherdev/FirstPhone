b23c6bffa98bf8ade5dded6cad9bffad65ec256a
lutherdev <devturd@gmail.com>
2025-06-30 08:54:52 +0800

    feat: App Update

--- app/src/main/java/com/example/firstphone/ReviewInfo.kt
+++ app/src/main/java/com/example/firstphone/ReviewInfo.kt
@@ -3,13 +3,22 @@ package com.example.firstphone
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class ReviewInfo : AppCompatActivity() {
     lateinit var btnBack: Button
+    lateinit var tvFirstName: TextView
+    lateinit var tvMiddleName: TextView
+    lateinit var tvLastName: TextView
+    lateinit var tvEmail: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +28,19 @@ class ReviewInfo : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        tvFirstName = findViewById<TextView>(R.id.tvFirstName)
+        tvMiddleName = findViewById<TextView>(R.id.tvMiddleName)
+        tvLastName = findViewById<TextView>(R.id.tvLastName)
+        tvEmail = findViewById<TextView>(R.id.tvEmail)
+
+        val bundle = intent.extras
+
+        tvFirstName.text = bundle?.getString("firstName")
+        tvMiddleName.text = bundle?.getString("middleName")
+        tvLastName.text = bundle?.getString("lastName")
+        tvEmail.text = bundle?.getString("email")
+
+
         btnBack = findViewById<Button>(R.id.btnSignBack)
         btnBack.setOnClickListener{
             val intent = Intent(this, SignUp::class.java)
--- app/src/main/java/com/example/firstphone/SignUp.kt
+++ app/src/main/java/com/example/firstphone/SignUp.kt
@@ -3,6 +3,8 @@ package com.example.firstphone
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,13 @@ import androidx.core.view.WindowInsetsCompat
 class SignUp : AppCompatActivity() {
     lateinit var btnLoginBack: Button
     lateinit var btnSignUp: Button
+    lateinit var etFirstName: EditText
+    lateinit var etMiddleName: EditText
+    lateinit var etLastName: EditText
+    lateinit var etEmail: EditText
+    lateinit var etPassword: EditText
+    lateinit var etConfirmPass: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,15 +29,41 @@ class SignUp : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         btnLoginBack = findViewById<Button>(R.id.btnLoginBack)
         btnSignUp = findViewById<Button>(R.id.btnSignupProceed)
+
+        etFirstName = findViewById<EditText>(R.id.etFirstName)
+        etMiddleName = findViewById<EditText>(R.id.etMiddleName)
+        etLastName = findViewById<EditText>(R.id.etLastname)
+        etEmail = findViewById<EditText>(R.id.etEmail)
+        etPassword = findViewById<EditText>(R.id.etPassword)
+        etConfirmPass = findViewById<EditText>(R.id.etConfirmPass)
+
+
+
         btnLoginBack.setOnClickListener{
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
         btnSignUp.setOnClickListener{
-            val intent = Intent(this, ReviewInfo::class.java)
-            startActivity(intent)
+
+
+            val bundle = Bundle()
+            bundle.putString("firstName", etFirstName.text.toString())
+            bundle.putString("middleName", etMiddleName.text.toString())
+            bundle.putString("lastName", etLastName.text.toString())
+            bundle.putString("email", etEmail.text.toString())
+            bundle.putString("password", etPassword.text.toString())
+            if (etPassword.text.toString().equals(etConfirmPass.text.toString())){
+                val intent = Intent(this, ReviewInfo::class.java)
+                intent.putExtras(bundle)
+                startActivity(intent)
+            }
+            else {
+                Toast.makeText(this, "Password does not match, please retype your pass", Toast.LENGTH_LONG).show()
+            }
+
         }
     }
 }
